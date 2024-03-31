5257bff52d1eae9b1df813adef43b5ad1e79de49
jaye <74174900+shiopao@users.noreply.github.com>
2024-03-31 17:31:31 +0800

    Add input and output in C

--- x86-to-C Interface/main.c
+++ x86-to-C Interface/main.c
@@ -5,11 +5,44 @@
 extern void stencil_x86(float* X, float* Y, int n);
 
 void stencil(float X[], float Y[], int n) {
-    for (int i = 3; i < n - 3; i++) {
+    for (int i = 3; i <= n - 3; i++) {
         Y[i] = X[i - 3] + X[i - 2] + X[i - 1] + X[i] + X[i + 1] + X[i + 2] + X[i + 3];
     }
 }
 
 int main() {
+    int n;
+
+    printf("Enter the size of vector (n): ");
+    scanf_s("%d", &n);
+
+    float* X = (float*)malloc(n * sizeof(float));
+    float* Y = (float*)malloc(n * sizeof(float));
+
+    for (int i = 0; i < n; i++) {
+        X[i] = i + 1;
+    }
+
+    stencil(X, Y, n);
+
+    printf("Vector Y (stencil function): ");
+    for (int i = 0; i < n; i++) {
+        printf("%.2f ", Y[i]);
+    }
+    printf("\n");
+
+    /*
+    stencil_x86(X, Y, n);
+
+    printf("Vector Y (stencil_x86 function): ");
+    for (int i = 0; i < n; i++) {
+        printf("%.2f ", Y[i]);
+    }
+    printf("\n"); 
+    */
+
+    free(X);
+    free(Y);
+
 	return 0;
 }
