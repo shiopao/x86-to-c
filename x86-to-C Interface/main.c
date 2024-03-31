fb2271075028e859211e2ffa365bb834c967f340
jaye <74174900+shiopao@users.noreply.github.com>
2024-03-31 16:59:06 +0800

    Add C and x86 code for 1-D stencil

--- x86-to-C Interface/main.c
+++ x86-to-C Interface/main.c
@@ -1,12 +1,15 @@
-// Template C to x86 call
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <windows.h>
 
-extern void asmhello();
+extern void stencil_x86(float* X, float* Y, int n);
+
+void stencil(float X[], float Y[], int n) {
+    for (int i = 3; i < n - 3; i++) {
+        Y[i] = X[i - 3] + X[i - 2] + X[i - 1] + X[i] + X[i + 1] + X[i + 2] + X[i + 3];
+    }
+}
 
 int main() {
-	asmhello();
 	return 0;
 }
--- x86-to-C Interface/source.asm
+++ x86-to-C Interface/source.asm
@@ -1,18 +1,46 @@
-; assembly part using x86-64
-
-section .data
-msg db "Hello World",13,10,0
-
 section .text
 bits 64
-default rel ; to handle address relocation
+default rel 
 
-global asmhello
-extern printf
+global stencil_x86
 
-asmhello:
-	sub rsp, 8*5 ; caller
-	mov rcx, msg
-	call printf
-	add rsp, 8*5
+stencil_x86:
+    ; Parameters
+    ; rdi: vector X
+    ; rsi: vector Y
+    ; rdx: number of elements in X and Y
+
+    MOV ecx, 3 ; loop counter (i = 3)
+
+stencil_loop:
+    MOV rax, rdi    ; base address of X
+    MOV r8, rsi     ; base address of Y
+    MOV r9, rdx     ; number of elements in X and Y
+    IMUL r9, rcx, 4 ; adjust loop to size of float
+    ADD rax, r9     ; address of X[i]
+    ADD r8, r9      ; address of Y[i]
+
+    ; Y[i] = X[i - 3] + X[i - 2] + X[i - 1] + X[i] + X[i + 1] + X[i + 2] + X[i + 3]
+    MOVSS xmm0, [rax - 12]  ; xmm0 = X[i - 3]
+    MOVSS xmm1, [rax - 8]   ; xmm1 = X[i - 2]
+    MOVSS xmm2, [rax - 4]   ; xmm2 = X[i - 1]
+    MOVSS xmm3, [rax]       ; xmm3 = X[i]
+    MOVSS xmm4, [rax + 4]   ; xmm4 = X[i + 1]
+    MOVSS xmm5, [rax + 8]   ; xmm5 = X[i + 2]
+    MOVSS xmm6, [rax + 12]  ; xmm6 = X[i + 3]
+    
+    ADDSS xmm0, xmm1 ; xmm0 = X[i - 3] + X[i - 2]
+    ADDSS xmm0, xmm2 ; xmm0 += X[i - 1]
+    ADDSS xmm0, xmm3 ; xmm0 += X[i]
+    ADDSS xmm0, xmm4 ; xmm0 += X[i + 1]
+    ADDSS xmm0, xmm5 ; xmm0 += X[i + 2]
+    ADDSS xmm0, xmm6 ; xmm0 += X[i + 3]
+
+    MOVSS [r8], xmm0 ; store result in Y[i]
+
+    INC ecx ; increment loop counter
+    
+    CMP ecx, edx 
+    JL stencil_loop ; continue if i < n - 3
+
 	ret
